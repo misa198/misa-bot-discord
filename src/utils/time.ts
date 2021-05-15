79184ea1ebbcc98cd5e34c4989e3bc22968b2aa6
misa198 <csnsjh@gmail.com>
2021-05-15 23:38:23 +0700

    add license

--- src/utils/time.ts
+++
@@ -1,6 +0,0 @@
-export const formatTimeRange = (timeRange: number): string => {
-  const hours = Math.floor(timeRange / 60);
-  const mins = timeRange - hours * 60;
-
-  return `${hours < 10 ? "0" + hours : hours}:${mins < 10 ? "0" + mins : mins}`;
-};
