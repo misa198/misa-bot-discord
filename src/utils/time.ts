52731670663d4db4dfa5ac94f3bcb9944e30593b
misa198 <csnsjh@gmail.com>
2021-05-15 20:38:59 +0700

    fix time range format

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,6 @@
 export const formatTimeRange = (timeRange: number): string => {
   const hours = Math.floor(timeRange / 60);
-  const mins = (timeRange - hours * 60);
+  const mins = timeRange - hours * 60;
 
-  return `${hours}:${mins}`;
+  return `${hours < 10 ? "0" + hours : hours}:${mins < 10 ? "0" + mins : mins}`;
 };
