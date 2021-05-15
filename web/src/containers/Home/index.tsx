2227d2d82914d329d1703509ef93000cf2ff55a2
misa198 <csnsjh@gmail.com>
2021-05-15 22:15:51 +0700

    home

--- web/src/containers/Home/index.tsx
+++ web/src/containers/Home/index.tsx
@@ -1,7 +1,39 @@
-import { HomeWrapper } from "./styled";
+import {
+  HomeWrapper,
+  Introduce,
+  IntroduceBrand,
+  IntroduceTitle,
+  IntroduceDescription,
+  InviteButtonWrapper,
+  InviteButton,
+  Banner,
+  BannerImage,
+} from "./styled";
+
+import BannerImageC from "../../assets/banner.png";
 
 const Home = () => {
-  return <HomeWrapper></HomeWrapper>;
+  return (
+    <HomeWrapper>
+      <Introduce>
+        <IntroduceTitle>{"NEW & IMPROVED"}</IntroduceTitle>
+        <IntroduceBrand>You, your friends, and some good tunes.</IntroduceBrand>
+        <IntroduceDescription>
+          Misa makes it possible to listen to your favorite music with all your
+          friends. Add it to Discord today and start listening!
+        </IntroduceDescription>
+        <InviteButtonWrapper
+          href="https://discord.com/api/oauth2/authorize?client_id=843003904904134667&permissions=2151079744&scope=bot"
+          target="__blank"
+        >
+          <InviteButton>Invite Misa</InviteButton>
+        </InviteButtonWrapper>
+      </Introduce>
+      <Banner>
+        <BannerImage src={BannerImageC} />
+      </Banner>
+    </HomeWrapper>
+  );
 };
 
 export default Home;
--- web/src/containers/Home/styled.ts
+++ web/src/containers/Home/styled.ts
@@ -1,5 +1,66 @@
 import styled from "styled-components";
 
 export const HomeWrapper = styled.div`
-  
+  display: flex;
+  align-items: center;
+  min-height: 80vh;
+  max-width: 1300px;
+  padding: 0 1rem;
+  margin: auto;
+`;
+
+export const Introduce = styled.div`
+  width: 60%;
+  padding: 0 72px 0 0;
+  margin: 0 16px 0 0;
+`;
+
+export const IntroduceTitle = styled.div`
+  color: #0099ff;
+  text-transform: uppercase;
+  font-weight: 500;
+  font-size: 17px;
+  letter-spacing: 1px;
+  line-height: 1.25;
+`;
+
+export const IntroduceBrand = styled.div`
+  color: #eef1f5;
+  font-weight: 900;
+  font-size: 35px;
+  line-height: 1.25;
+  margin: 8px 0;
+`;
+
+export const IntroduceDescription = styled.p`
+  color: #bec3cd;
+  font-size: 0.9rem;
+  margin-top: 0.75rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const InviteButtonWrapper = styled.a``;
+
+export const InviteButton = styled.button`
+  background: linear-gradient(
+    90deg,
+    rgba(0, 149, 238, 1) 0%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  color: #eef1f5;
+  padding: 14px 28px;
+  border-radius: 4px;
+  font-weight: 600;
+  display: inline-block;
+  transition: color 0.2s ease-in-out, background 0.2s ease-in-out;
+  border: none;
+  cursor: pointer;
+`;
+
+export const Banner = styled.div`
+  width: 40%;
+`;
+
+export const BannerImage = styled.img`
+  width: 100%;
 `;
