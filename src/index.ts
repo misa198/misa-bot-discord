394608b4055275db3e1987bb3c81c6a1be46b0e1
misa198 <csnsjh@gmail.com>
2021-05-15 13:12:52 +0700

    setup bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
+import { config } from "dotenv";
+config();
 
+import { Client } from "discord.js";
+
+import { prefix } from "./constant/config";
+
+const client = new Client();
+const token = process.env.TOKEN;
+
+client.on("ready", () => {
+  console.log("Misabox is online!");
+});
+
+client.on("message", (message) => {
+  console.log(message);
+});
+
+client.login(token);
