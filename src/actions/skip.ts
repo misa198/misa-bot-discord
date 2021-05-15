b8267d7e9471b6bd761f13171c2e33d459d4980c
misa198 <csnsjh@gmail.com>
2021-05-15 18:01:34 +0700

    update skip

--- src/actions/skip.ts
+++ src/actions/skip.ts
@@ -1,5 +1,6 @@
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 
+import { formatTimeRange } from "../utils/time";
 import { servers } from "../data/server";
 
 export default {
@@ -7,12 +8,32 @@ export default {
   execute: (message: Message): void => {
     const server = servers[message.guild.id];
     if (server) {
-      if (server.queue.length === 0) {
-        message.channel.send("❌ Nothing to skip!");
-      }
       if (server.dispatcher) {
-        server.dispatcher.end();
-      }
+        if (server.queue.length === 0) {
+          server.dispatcher.end();
+          message.channel.send("❌ Nothing to skip!");
+        } else {
+          const song = server.queue[0];
+          const messageEmbed = new MessageEmbed()
+            .setColor("#0099ff")
+            .setTitle(song.resource.title)
+            .setAuthor(`Skipped`)
+            .setThumbnail(song.resource.thumbnail)
+            .addFields(
+              { name: "Channel", value: song.resource.author, inline: true },
+              {
+                name: "Length",
+                value: formatTimeRange(song.resource.length),
+                inline: true,
+              }
+            )
+            .setImage(song.resource.avatar);
+
+          message.channel
+            .send(messageEmbed)
+            .then(() => server.dispatcher.end());
+        }
+      } else message.channel.send("❌ Nothing to skip!");
     } else {
       message.channel.send("❌ Nothing to skip!");
     }
