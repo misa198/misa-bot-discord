00b4c9ef0e5a4cfd50c815e57d52ab3bf0b31f8a
Thanh Vu <csnsjh@gmail.com>
2021-09-14 22:57:45 +0700

    update

--- src/__tests__/services/youtube.test.ts
+++ src/__tests__/services/youtube.test.ts
@@ -5,7 +5,7 @@ describe('Get video details with keyword', () => {
   it('Return song object with valid url', async () => {
     const keyword = 'Shape of you';
     const result = await YoutubeService.getVideoDetails(keyword);
-    expect(result.url).not.equal('');
+    expect(result?.url).not.equal('');
   });
 });
 
@@ -13,7 +13,7 @@ describe('Get video details with url', () => {
   it('Return song object with valid url', async () => {
     const url = 'https://www.youtube.com/watch?v=JGwWNGJdvx8';
     const result = await YoutubeService.getVideoDetails(url);
-    expect(result.url).not.equal('');
+    expect(result?.url).not.equal('');
   });
 });
 
@@ -22,8 +22,8 @@ describe('Get playlist with url', () => {
     const url =
       'https://www.youtube.com/playlist?list=PLjp0AEEJ0-fGs1_EVsLd8efDByQ9GnvTq';
     const result = await YoutubeService.getPlaylist(url);
-    expect(result.songs.length).greaterThan(0);
-    expect(result.songs[0].title).not.equal('');
-    expect(result.title).not.equal('');
+    expect(result?.songs.length).greaterThan(0);
+    expect(result?.songs[0].title).not.equal('');
+    expect(result?.title).not.equal('');
   });
 });
