9013a1f0f32283a446cde7876120c09dc0d4cddb
Thanh Vu <csnsjh@gmail.com>
2021-09-14 22:56:22 +0700

    update

--- src/services/youtube.ts
+++ src/services/youtube.ts
@@ -1,5 +1,4 @@
 import { youtubeVideoRegex } from '@/constants/regex';
-import { Error } from '@/models/Error';
 import { Playlist } from '@/types/Playlist';
 import { Platform, Song } from '@/types/Song';
 import ytdl from 'ytdl-core';
@@ -7,57 +6,66 @@ import ytpl from 'ytpl';
 import ytsr, { Video } from 'ytsr';
 
 export class YoutubeService {
-  public static async getVideoDetails(content: string): Promise<Song> {
+  public static async getVideoDetails(content: string): Promise<Song | null> {
     const parsedContent = content.match(youtubeVideoRegex);
     let id = '';
     if (!parsedContent) {
-      id = await this.searchVideo(content);
+      const result = await this.searchVideo(content);
+      if (!result) return null;
+      id = result;
     } else {
       id = parsedContent[1];
     }
     const videoUrl = this.generateVideoUrl(id);
-    const result = await ytdl.getInfo(videoUrl);
-    return {
-      title: result.videoDetails.title,
-      length: parseInt(result.videoDetails.lengthSeconds, 10),
-      author: result.videoDetails.author.name,
-      thumbnail:
-        result.videoDetails.thumbnails[
-          result.videoDetails.thumbnails.length - 1
-        ].url,
-      url: videoUrl,
-      platform: Platform.YOUTUBE,
-    };
-  }
-
-  public static async getPlaylist(url: string): Promise<Playlist> {
-    const id = url.split('?')[1].split('=')[1];
-    const playlist = await ytpl(id);
-
-    const songs: Song[] = [];
-    playlist.items.forEach((item) => {
-      songs.push({
-        title: item.title,
-        thumbnail: item.bestThumbnail.url || '',
-        author: item.author.name,
-        url: item.shortUrl,
-        length: item.durationSec || 0,
+    try {
+      const result = await ytdl.getInfo(videoUrl);
+      return {
+        title: result.videoDetails.title,
+        length: parseInt(result.videoDetails.lengthSeconds, 10),
+        author: result.videoDetails.author.name,
+        thumbnail:
+          result.videoDetails.thumbnails[
+            result.videoDetails.thumbnails.length - 1
+          ].url,
+        url: videoUrl,
         platform: Platform.YOUTUBE,
-      });
-    });
-
-    return {
-      title: playlist.title,
-      thumbnail: playlist.bestThumbnail.url || '',
-      author: playlist.author.name,
-      songs,
-    };
+      };
+    } catch (e) {
+      return null;
+    }
   }
 
-  private static async searchVideo(keyword: string): Promise<string> {
+  public static async getPlaylist(url: string): Promise<Playlist | null> {
+    try {
+      const id = url.split('?')[1].split('=')[1];
+      const playlist = await ytpl(id);
+      const songs: Song[] = [];
+      playlist.items.forEach((item) => {
+        songs.push({
+          title: item.title,
+          thumbnail: item.bestThumbnail.url || '',
+          author: item.author.name,
+          url: item.shortUrl,
+          length: item.durationSec || 0,
+          platform: Platform.YOUTUBE,
+        });
+      });
+
+      return {
+        title: playlist.title,
+        thumbnail: playlist.bestThumbnail.url || '',
+        author: playlist.author.name,
+        songs,
+      };
+    } catch (e) {
+      return null;
+    }
+  }
+
+  private static async searchVideo(keyword: string): Promise<string | null> {
     const result = await ytsr(keyword, { pages: 1 });
     const filteredRes = result.items.filter((item) => item.type === 'video');
-    if (filteredRes.length === 0) throw new Error("🔎 Can't find video!");
+    if (filteredRes.length === 0) return null;
     const item = filteredRes[0] as Video;
     return item.id;
   }
