3550d0c7e0d65ea94c218e31bb18d01798fac023
Thanh Vu <csnsjh@gmail.com>
2021-09-14 23:21:11 +0700

    server

--- src/models/Server.ts
+++ src/models/Server.ts
@@ -1,6 +1,12 @@
 import {} from '@discordjs/voice';
 import { Song } from '@/types/Song';
 
+interface QueueItem {
+  song: Song;
+  requester: string;
+}
+
 export class Server {
   playing?: Song;
+  queue: QueueItem[] = [];
 }
